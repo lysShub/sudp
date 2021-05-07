84c788bbcee7b9d1f32311b06d8ce9da8728994c
lysShub <2454148255@qq.com>
2021-05-07 16:49:42 +0800

    lysShub

--- test/sever/test.go
+++ test/sever/test.go
@@ -13,7 +13,7 @@ func main() {
 	t.Addr = &net.UDPAddr{IP: net.ParseIP("192.168.0.50"), Port: 19986} //192.168.0.50
 	t.Encrypto = false
 	// t.Path = `/root/workspace/sudp/tmp/r/DSPdsign.7z`
-	t.Path = `/root/workspace/sudp/tmp/r/tele.apk`
+	t.Path = `/root/workspace/sudp/tmp/r/Telegram.apk`
 
 	fmt.Println(t.Send(func(requestBody []byte) bool {
 		return true
